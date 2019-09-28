269ca4aee505b82a4b1086b6ba2f5b785f5b1988
Mehmet Tarhan <memtarhan@gmail.com>
2019-09-28 10:54:10 +0300

    Mastering ARKit - World Tracking - Resetting the World Origin

--- MasteringARKit/1-World-Tracking/README.md
+++ MasteringARKit/1-World-Tracking/README.md
@@ -2,5 +2,25 @@
 
 ### How World Tracking Works in ARKit 
 
-ARKit uses a coordinate system where the x-axis points left and right, the y-axis points up and down, and the z-axis points toward and away from the camera.
+> ARKit uses a coordinate system where the x-axis points left and right, the y-axis points up and down, and the z-axis points toward and away from the camera.
+
+
+
+### Resetting the World Origin 
+
+> Each time an AR app is run, world coordinates are defined at the current location of the iOS device. You may want to change those coordinates so ARKit provides a solution for that.
+
+
+
+```swift
+@IBOutlet var sceneView: ARSCNView!
+    
+private var configuration: ARWorldTrackingConfiguration!
+
+// MARK: - Resetting the World Origin
+private func resetWorldOrigin() {
+     sceneView.session.pause()
+     sceneView.session.run(configuration, options: [.resetTracking])
+}
+```
 
--- MasteringARKit/1-World-Tracking/SampleApp/SampleApp/ViewController.swift
+++ MasteringARKit/1-World-Tracking/SampleApp/SampleApp/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
+    
+    private var configuration: ARWorldTrackingConfiguration!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +39,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -49,30 +51,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    @IBAction func resetButtonDidTap(_ sender: Any) {
+        resetWorldOrigin()
     }
-*/
     
+    // MARK: - Resetting the World Origin
+    private func resetWorldOrigin() {
+        print(#function)
+        sceneView.session.pause()
+        sceneView.session.run(configuration, options: [.resetTracking])
+    }
+}
+
+// MARK: - ARSCNViewDelegate
+
+extension ViewController: ARSCNViewDelegate {
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
+        print(#function)
     }
     
     func sessionWasInterrupted(_ session: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+        print(#function)
     }
     
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+        print(#function)
+    }
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        print(#function)
     }
 }

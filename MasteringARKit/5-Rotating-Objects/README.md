49a9a2a85a2dc34f6ec0b2f2a05ddcadab454b9b
Mehmet Tarhan <memtarhan@gmail.com>
2019-11-01 16:59:30 +0300

    Mastering ARKit - Rotating Objects - Rotating Objects Using Euler Angles

--- MasteringARKit/5-Rotating-Objects/README.md
+++ MasteringARKit/5-Rotating-Objects/README.md
@@ -15,3 +15,40 @@ import GLKit
 let radian = GLKMathDegreesToRadians(degrees)
 ```
 
+
+
+```swift
+import GLKit
+
+ private var node = SCNNode()
+    
+ private var currentX: Float = 0
+ private var currentY: Float = 0
+ private var currentZ: Float = 0
+
+ @IBAction func xChanged(_ sender: UISlider) {
+        currentX = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+  }
+    
+  @IBAction func yChanged(_ sender: UISlider) {
+        currentY = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+  }
+    
+  @IBAction func zChanged(_ sender: UISlider) {
+        currentZ = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+  }
+    
+  private func showShape() {
+        let pyramid = SCNPyramid(width: 0.05, height: 0.1, length: 0.05)
+        pyramid.firstMaterial?.diffuse.contents = UIColor.orange
+        
+        node.geometry = pyramid
+        node.position = SCNVector3(0.05, 0.05, -0.05)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+ }
+```
+
--- MasteringARKit/5-Rotating-Objects/SampleApp/SampleApp/ViewController.swift
+++ MasteringARKit/5-Rotating-Objects/SampleApp/SampleApp/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 import SceneKit
 import ARKit
+import GLKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var xSlider: UISlider!
+    @IBOutlet weak var ySlider: UISlider!
+    @IBOutlet weak var zSlider: UISlider!
+    
+    private var node = SCNNode()
+    
+    private var currentX: Float = 0
+    private var currentY: Float = 0
+    private var currentZ: Float = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +33,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // Set debug options
+        sceneView.debugOptions = [.showWorldOrigin, .showFeaturePoints]
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -38,6 +51,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
+        showShape()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -47,6 +62,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    @IBAction func xChanged(_ sender: UISlider) {
+        currentX = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+    }
+    
+    @IBAction func yChanged(_ sender: UISlider) {
+        currentY = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+    }
+    
+    @IBAction func zChanged(_ sender: UISlider) {
+        currentZ = GLKMathDegreesToRadians(sender.value)
+        node.eulerAngles = SCNVector3(currentX, currentY, currentZ)
+    }
+    
+    private func showShape() {
+        let pyramid = SCNPyramid(width: 0.05, height: 0.1, length: 0.05)
+        pyramid.firstMaterial?.diffuse.contents = UIColor.orange
+        
+        node.geometry = pyramid
+        node.position = SCNVector3(0.05, 0.05, -0.05)
+        
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
     // MARK: - ARSCNViewDelegate
     
 /*

07b81bc0c4e49e3ffc9600a06a6991f0e189e4c1
Mehmet Tarhan <memtarhan@gmail.com>
2019-11-02 18:34:16 +0300

    Mastering ARKit - Interacting - Created sample app

---
+++ MasteringARKit/8-Interacting/SampleApp/SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by Mehmet Tarhan Personal on 2.11.2019.
+//  Copyright © 2019 Mehmet Tarhan Personal. All rights reserved.
+//
+
+import XCTest
+@testable import SampleApp
+
+class SampleAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

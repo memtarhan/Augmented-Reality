3766094231d46b6596f8d73cf374ffc0cd985f7e
Mehmet Tarhan <memtarhan@gmail.com>
2019-12-24 18:38:20 +0300

    Mastering ARKit - Physics on Virtual Object - Physical body and attributes

---
+++ Mastering ARKit/10-Physics-on-Virtual-Objects/README.md
@@ -0,0 +1,13 @@
+## Physics on Virtual Objects
+
+> You can define how a virtual object interacts with others(static, dynamic, or kinematic), the shape of a virtual object such as a box or sphere, and the force applied to a virtual object. 
+
+A virtual object can behave on one of three ways: 
+
+* **Static - ** Cannot move and is unaffected by collision or force
+* **Dynamic - **Affected by forces and collisions. 
+* **Kinematic - **Not affected by forces or collisions, but can affect dynamic virtual objects. 
+  * In a video game a road can be example of kinematics. 
+
+
+
--- Mastering ARKit/10-Physics-on-Virtual-Objects/SampleApp/SampleApp/ViewController.swift
+++ Mastering ARKit/10-Physics-on-Virtual-Objects/SampleApp/SampleApp/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 import SceneKit
 import ARKit
+import GLKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
+    
+    private var configuration: ARWorldTrackingConfiguration!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +27,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -33,9 +36,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        configuration = ARWorldTrackingConfiguration()
+        
+        // Set detection type
+        configuration.planeDetection = .horizontal
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -46,17 +51,66 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    @IBAction func refreshDidTap(_ sender: UIBarButtonItem) {
+        // Refreshing world tracking and removing all the nodes
+        sceneView.scene.rootNode.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        
+        sceneView.session.run(configuration, options: [.resetTracking])
+    }
+    
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+        guard let scene = sender.view as? ARSCNView else { return }
+        let tappedLocation = sender.location(in: scene)
+        let hitTest = scene.hitTest(tappedLocation, types: .existingPlaneUsingExtent)
+        guard let result = hitTest.first else { return }
+        addObject(with: result)
+    }
+    
+    private func addObject(with result: ARHitTestResult) {
         let node = SCNNode()
-     
+        node.geometry = SCNSphere(radius: 0.1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(result.worldTransform.columns.3.x,
+                                   result.worldTransform.columns.3.y,
+                                   result.worldTransform.columns.3.z)
+        // IMPL: shape: nil
+        // ARKit will treat the sphere’s boundaries as its body when calculating collisions with other virtual objects.
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    private func displayPlane(on anchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNNode()
+        node.geometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        node.geometry?.firstMaterial?.isDoubleSided = true
+        node.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        let ninetyDegrees = GLKMathDegreesToRadians(90)
+        node.eulerAngles = SCNVector3(ninetyDegrees, 0, 0)
+        node.physicsBody = SCNPhysicsBody(type: .kinematic, shape: nil)
+        
         return node
     }
-*/
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        let plane = displayPlane(on: anchor as! ARPlaneAnchor)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARPlaneAnchor else { return }
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let plane = displayPlane(on: anchor as! ARPlaneAnchor)
+        node.addChildNode(plane)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user

e073d2b7afd19cc0235264d8100b40bd560f9b22
Mehmet Tarhan <contact@memtarhan.com>
2020-02-29 19:06:32 +0300

    Augmented Words - Created project

---
+++ AugmentedWords/AugmentedWordsTests/AugmentedWordsTests.swift
@@ -0,0 +1,34 @@
+//
+//  AugmentedWordsTests.swift
+//  AugmentedWordsTests
+//
+//  Created by Mehmet Tarhan on 29.02.2020.
+//  Copyright © 2020 Mehmet Tarhan. All rights reserved.
+//
+
+import XCTest
+@testable import AugmentedWords
+
+class AugmentedWordsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

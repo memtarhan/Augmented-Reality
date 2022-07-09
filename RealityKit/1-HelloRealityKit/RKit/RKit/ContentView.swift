5f8f967d3b39b539aae73b487bcc329dc96da0ae
Mehmet Tarhan <hi@memtarhan.com>
2022-07-09 23:14:11 +0100

    [RealityKit] - Creating a metallic box

--- RealityKit/1-HelloRealityKit/RKit/RKit/ContentView.swift
+++ RealityKit/1-HelloRealityKit/RKit/RKit/ContentView.swift
@@ -5,39 +5,38 @@
 //  Created by Mehmet Tarhan on 09/07/2022.
 //
 
-import SwiftUI
 import RealityKit
+import SwiftUI
 
-struct ContentView : View {
+struct ContentView: View {
     var body: some View {
         return ARViewContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
-    
     func makeUIView(context: Context) -> ARView {
-        
         let arView = ARView(frame: .zero)
-        
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+
+        let anchor = AnchorEntity(plane: .horizontal)
+
+        let material = SimpleMaterial(color: .blue, isMetallic: true)
+        let meshResource = MeshResource.generateBox(size: 0.3)
+        let box = ModelEntity(mesh: meshResource, materials: [material])
+
+        anchor.addChild(box)
+        arView.scene.anchors.append(anchor)
         
         return arView
-        
     }
-    
+
     func updateUIView(_ uiView: ARView, context: Context) {}
-    
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
 #endif

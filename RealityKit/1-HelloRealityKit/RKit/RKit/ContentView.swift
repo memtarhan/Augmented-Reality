46848e39666ce564396aacb8fd7248fdb45af4a0
Mehmet Tarhan <hi@memtarhan.com>
2022-07-09 23:22:57 +0100

    [RealityKit] - Multiple entities

--- RealityKit/1-HelloRealityKit/RKit/RKit/ContentView.swift
+++ RealityKit/1-HelloRealityKit/RKit/RKit/ContentView.swift
@@ -21,10 +21,18 @@ struct ARViewContainer: UIViewRepresentable {
         let anchor = AnchorEntity(plane: .horizontal)
 
         let material = SimpleMaterial(color: .blue, isMetallic: true)
-        let meshResource = MeshResource.generateBox(size: 0.3)
+        let meshResource = MeshResource.generateBox(size: 0.1)
         let box = ModelEntity(mesh: meshResource, materials: [material])
 
         anchor.addChild(box)
+        
+        let sphere = ModelEntity(mesh: MeshResource.generateSphere(radius: 0.1), materials: [SimpleMaterial(color: .yellow, isMetallic: true)])
+        sphere.position = simd_make_float3(0, 0.3, 0)
+        anchor.addChild(sphere)
+        
+        let plane = ModelEntity(mesh: MeshResource.generatePlane(width: 0.5, depth: 0.3), materials: [SimpleMaterial(color: .black, isMetallic: true)])
+        anchor.addChild(plane)
+        
         arView.scene.anchors.append(anchor)
         
         return arView
